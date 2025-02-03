a914e88de291c68a1be5464a5a9d250dbf2e31d3
Irmak Kahya <138292432+irmakka@users.noreply.github.com>
2025-02-03 17:52:56 +0300

    Update LibraryFrame.java

--- LibraryFrame.java
+++ LibraryFrame.java
@@ -9,7 +9,8 @@ public class   LibraryFrame extends JFrame {
     private JLabel resultLabel;
     private   final Library library;
     private JLabel label;
-  
+    private  final JButton darkButton;
+    private final JButton whiteButton;
 
     public LibraryFrame(Library library){
         this.library=library;
@@ -24,6 +25,13 @@ public class   LibraryFrame extends JFrame {
         searchField= new JTextField(20);
          searchButton=new JButton("Search");
          resultLabel= new JLabel("Searching");
+        darkButton= new JButton("Dark Mode");
+        whiteButton= new JButton("White Mode");
+        darkButton.setBackground(new Color(90, 92, 100));
+        darkButton.setForeground(Color.WHITE);
+        whiteButton.setBackground(Color.LIGHT_GRAY);
+        whiteButton.setForeground(Color.BLACK);
+
         searchButton.addActionListener((ActionEvent e) -> {
             if (resultLabel.getText() != null && !resultLabel.getText().isEmpty()) {
                 resultLabel.setText("");
@@ -34,6 +42,25 @@ public class   LibraryFrame extends JFrame {
             resultLabel.setText(result);
             contentPane.add(resultLabel);
         });
+
+        
+        darkButton.addActionListener((ActionEvent e )-> {
+            contentPane.setBackground( new Color(65,67,75));
+            label.setForeground(Color.WHITE);
+            resultLabel.setForeground(Color.WHITE);
+        } );
+        
+        whiteButton.addActionListener( (ActionEvent e)->{
+            contentPane.setBackground(Color.WHITE);
+            label.setForeground(Color.BLACK);
+            resultLabel.setForeground(Color.BLACK);
+
+
+        });
+
+        
+            contentPane.add(whiteButton);
+            contentPane.add(darkButton);
             contentPane.add(label);
             contentPane.add(searchField);
             contentPane.add(searchButton);
